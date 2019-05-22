50acf6658eaa70949b944f6ff898bb8c2984d28d
Sebastian Fudalej <sebastianfudalej@gmail.com>
2019-05-22 21:54:14 +0200

    Reading data from file

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const axios = require('axios');
-const xlsx = require('xlsx');
+const XLSX = require('xlsx');
 const fs = require('fs');
+
+const dataDirectory = './data';
+
+fs.readdir('data', (err, files) => {
+  files.forEach(fileElement => {
+    const workbook = XLSX.readFile(dataDirectory + '/' + fileElement);
+    const sheetNameList = workbook.SheetNames;
+    const odsData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNameList[0]]);
+    odsData.forEach(element => {
+      console.log(element.miasto);
+    });
+  });
+})
